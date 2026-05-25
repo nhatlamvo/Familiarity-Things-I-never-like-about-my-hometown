b0d4194eeccead2bd8cce7e34f22226881ad8cee
nhatlamvo <146066794+nhatlamvo@users.noreply.github.com>
2026-05-25 11:21:19 +0700

    Update overlay.js
    260525

--- overlay.js
+++ overlay.js
@@ -1,4 +1,4 @@
-const modalBtns = document.querySelectorAll(".closes button"); // Select all close buttons
+const modalBtns = document.querySelectorAll(".close-btn-overlay"); // Select all close buttons
 const openModal1 = document.getElementById("piece01"); // phrases from the list
 const openModal2 = document.getElementById("piece02");
 const openModal3 = document.getElementById("piece03");
@@ -9,16 +9,16 @@ const openModal7 = document.getElementById("piece07");
 const openModal8 = document.getElementById("piece08");
 const openModal9 = document.getElementById("piece09");
 const openModal10 = document.getElementById("piece10");
-const modal1 = document.getElementById("video01");
-const modal2 = document.getElementById("video02");
-const modal3 = document.getElementById("video03");
-const modal4 = document.getElementById("video04");
-const modal5 = document.getElementById("video05");
-const modal6 = document.getElementById("video06");
-const modal7 = document.getElementById("video07");
-const modal8 = document.getElementById("video08");
-const modal9 = document.getElementById("video09");
-const modal10 = document.getElementById("video10");
+const modal1 = document.getElementById("page1");
+const modal2 = document.getElementById("page2");
+const modal3 = document.getElementById("page3");
+const modal4 = document.getElementById("page4");
+const modal5 = document.getElementById("page5");
+const modal6 = document.getElementById("page6");
+const modal7 = document.getElementById("page7");
+const modal8 = document.getElementById("page8");
+const modal9 = document.getElementById("page9");
+const modal10 = document.getElementById("page10");
 let curModal = null;
 
 //Function to show a specific modal
@@ -31,8 +31,10 @@ function showModal(modalId) {
 function hideModal() {
   if (curModal) {
     curModal.style.display = "none";
-    currentAudio.pause(); /*stop current audio when closing the overlay and */
-    currentAudio = null; /*allow the new audio to play when clicked */
+    if (currentAudio) {
+      currentAudio.pause(); /*stop current audio when closing the overlay and */
+      currentAudio = null; /*allow the new audio to play when clicked */
+    }
     // currentAudio.currentTime = 0;
   }
 }
@@ -43,34 +45,34 @@ modalBtns.forEach((btn) => {
 });
 
 // Open modal 1 on button click
-openModal1.addEventListener("click", () => showModal("video01"));
+openModal1.addEventListener("click", () => showModal("page1"));
 
 // Open modal 2 on button click
-openModal2.addEventListener("click", () => showModal("video02"));
+openModal2.addEventListener("click", () => showModal("page2"));
 
 // Open modal 3 on button click
-openModal3.addEventListener("click", () => showModal("video03"));
+openModal3.addEventListener("click", () => showModal("page3"));
 
 // Open modal 4 on button click
-openModal4.addEventListener("click", () => showModal("video04"));
+openModal4.addEventListener("click", () => showModal("page4"));
 
 // Open modal 5 on button click
-openModal5.addEventListener("click", () => showModal("video05"));
+openModal5.addEventListener("click", () => showModal("page5"));
 //////////////////////////////////
 // Open modal 6 on button click
-openModal6.addEventListener("click", () => showModal("video06"));
+openModal6.addEventListener("click", () => showModal("page6"));
 
 // Open modal 7 on button click
-openModal7.addEventListener("click", () => showModal("video07"));
+openModal7.addEventListener("click", () => showModal("page7"));
 
 // Open modal 8 on button click
-openModal8.addEventListener("click", () => showModal("video08"));
+openModal8.addEventListener("click", () => showModal("page8"));
 
 // Open modal 9 on button click
-openModal9.addEventListener("click", () => showModal("video09"));
+openModal9.addEventListener("click", () => showModal("page9"));
 
 // Open modal 10 on button click
-openModal10.addEventListener("click", () => showModal("video10"));
+openModal10.addEventListener("click", () => showModal("page10"));
 
 // //////////////////////////////////////////////////////
 // // Close modal on button click (can be looped for all close buttons)
